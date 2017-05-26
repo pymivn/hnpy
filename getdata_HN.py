f727830cf49483d73b11227c3425062f9ea41950
Hung Nguyen Viet <hvn@familug.org>
2017-05-26 13:43:04 +0700

    Refactor log, save the right dict, handle empty file correctly

--- getdata_HN.py
+++ getdata_HN.py
@@ -8,14 +8,20 @@ from urllib.parse import urljoin
 URL = 'https://hacker-news.firebaseio.com/v0/'
 FILE = 'data.json'
 
+logging.basicConfig(filename='getdata.log', level=logging.DEBUG)
+logging.getLogger("requests").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
 
 def _get_json(param):
     url = urljoin(URL, param)
     try:
         res = requests.get(url)
     except requests.exceptions.ConnectionError as err:
-        logging.error(err)
-    return res.json()
+        logger.error(url, err)
+        return {}
+    else:
+        return res.json()
 
 
 def _get_stories_of_topics(topics=None):
@@ -25,10 +31,13 @@ def _get_stories_of_topics(topics=None):
     for story_id in story_ids:
         param_id = 'item/{}.json?print=pretty'.format(story_id)
         story = _get_json(param_id)
+        if not story:
+            continue
         story_title = story.get('title', '')
+        story_url = story.get('url', '#')
+        logger.debug("Checking story %s: %s", story_title, story_url)
         for kwd in topics:
             if kwd in story_title.lower():
-                story_url = story.get('url', '#')
                 stories[str(story_id)] = {
                     'title': story_title,
                     'url': story_url
@@ -44,18 +53,18 @@ def _save_stories(stories):
 
 def main():
     start_time = time.time()
-    logging.basicConfig(filename='getdata.log', level=logging.WARNING)
     topics = ['python', 'django']
-    with open(FILE) as json_data:
-        stories = _get_stories_of_topics(topics)
-        try:
+    try:
+        with open(FILE) as json_data:
+            stories = _get_stories_of_topics(topics)
             current_data = json.load(json_data)
-        except IOError as err:
-            current_data = {}
-            logging.error(err)
-        current_data.update(stories)
-    _save_stories(stories)
-    logging.warning("--Completed in %s seconds." % (time.time() - start_time))
+    except IOError as err:
+        current_data = {}
+        logging.error("%s", err)
+
+    current_data.update(stories)
+    _save_stories(current_data)
+    logging.warning("--Completed in %s seconds.", (time.time() - start_time))
 
 
 if __name__ == "__main__":

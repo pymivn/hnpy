37aebea22800595617c98b172af64a71f4922270
Viet Hung Nguyen <hvn@familug.org>
2017-10-18 15:29:26 +0700

    Refactor python and use same code for HN top 200+points

--- slack_sender.py
+++ slack_sender.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import boto3
@@ -6,23 +7,30 @@ import requests
 import hnpy
 import util
 
+logger = logging.getLogger(__name__)
 
-def score_filter(s):
+
+def greater_42(s):
     return s['score'] > 42
 
 
-def slack_send_new_story(url, datapath='/tmp/data.csv', nosend=False):
+def slack_send_new_story(url, datapath='/tmp/data.csv',
+                         nosend=False,
+                         topics=None,
+                         filter_=None):
     if not url:
         nosend = True
 
     current_data = util.read_csv(datapath)
     print("Data loaded from existing db: %s" % current_data)
 
-    newstories = hnpy.get_stories_on_topic(hnpy.PYTHON_KEYWORDS)
+    newstories = hnpy.get_stories_on_topic(topics)
+
+    logger.info("Filtering on topic %s", topics)
 
     for_print = []
     for s in util.write_new_stories(newstories, current_data,
-                                    datapath, score_filter):
+                                    datapath, filter_):
 
         message = "{} points: {} - {} ({})".format(
             s['score'], s['title'], s['url'], util.get_hn_url(s['id'])
@@ -43,31 +51,46 @@ def slack_send_new_story(url, datapath='/tmp/data.csv', nosend=False):
         print(msg)
 
 
-def lambda_handler(event, context):
-    url = os.environ["SLACK_HOOK"]
-    datapath = '/tmp/data.csv'
+def download_s3_and_send(filename, topics, filter_, slack_url=None):
+    url = os.environ.get("SLACK_HOOK", '')
+    datapath = '/tmp/' + filename
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('hn.pymi.vn')
     try:
-        s3.Object('hn.pymi.vn', 'data.csv').load()
+        s3.Object('hn.pymi.vn', filename).load()
     except Exception as e:
         with open(datapath, 'w') as f:
-            f.write()
+            f.write('')
     else:
-        bucket.download_file('data.csv', datapath)
+        bucket.download_file(filename, datapath)
 
-    slack_send_new_story(url, datapath)
+    slack_send_new_story(url, datapath, topics=topics, filter_=filter_)
 
     bucket.put_object(ACL='public-read',
                       Bucket='hn.pymi.vn',
-                      Key='data.csv',
+                      Key=filename,
                       Body=open(datapath).read().encode(),
                       )
 
 
+def lambda_handler(event, context):
+    filename = 'data.csv'
+    download_s3_and_send(filename,
+                         topics=hnpy.PYTHON_KEYWORDS,
+                         filter_=greater_42)
+
+
+def send_top_hn(event, context):
+    filename = 'top.csv'
+    download_s3_and_send(filename,
+                         topics=None,
+                         filter_=lambda s: s['score'] > 200)
+
+
 def test():
-    url = os.environ.get("SLACK_HOOK", '')
-    slack_send_new_story(url, datapath='data.csv')
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    send_top_hn(None, None)
 
 
 if __name__ == "__main__":
